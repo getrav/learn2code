3789a81ac12e16588ab55abbb484c39f333f669a
Hamaddogar <hamadali77115033@gmail.com>
2020-03-25 23:47:51 +0500

    3/25/2020

---
+++ js/myjs.js
@@ -0,0 +1,8 @@
+function myfunction() {
+  var myvalue = prompt(" Enter Number");
+  if (myvalue > 20) {
+    alert("value is greater than 20");
+  } else {
+    alert("value is less than 20");
+  }
+}
